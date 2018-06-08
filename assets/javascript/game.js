ea6e98105ad2ae1f70b3607f73b65ece98f4a050
smiley-coyote <nick.a.alex@gmail.com>
2018-06-08 19:59:23 -0400

    basic game is finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -104,51 +104,245 @@ $("#start-text").text("Select your fighter!");
 
     }
 
-    // create a function that works with onClick if gameStart == true
+    // fighting conditions
    
     $("#start-text").on("click", function() {
         if (gameStart == true) {
-            // air 
+            // if you're Aang (air) ===================================================================================
             if (yourFighter == fighter.airbender) {
-                // vs water
+                // vs Katara (water)
                 if (computerFighter == fighter.waterbender) {
-                    computerFighter.hp = (computerFighter.hp - (yourFighter.ap + 5));
-                    yourFighter.ap = yourFighter.ap + 6;
-                    $("#stats-you").text("Aang attacked Katara with " + yourFighter.ap + " ap!")
-                    var i = Math.floor(Math.random() * 3);
-                    console.log(i);
-                    if (i < 2) {
+                    var j = Math.floor(Math.random() * 6);
+                    var i = Math.floor(Math.random() * 5);
+                    if (j < 5) {
+                        computerFighter.hp = (computerFighter.hp - (yourFighter.ap + 5));
+                        yourFighter.ap = yourFighter.ap + 5;
+                        $("#stats-you").text("Aang attacked Katara with " + yourFighter.ap + " ap!")
+                    } else {
+                        $("#stats-you").text("Aang's attack missed!");
+                    }
+                    if (i < 4) {
                     yourFighter.hp = (yourFighter.hp - (computerFighter.cap-5));
                     $("#stats-computer").text("Katara counter-attacked with " + (computerFighter.cap-5) + " ap!")
                     } else {
                         $("#stats-computer").text("Katara's counter-attack missed!")
                     }
                 }
-                // vs fire
+                // vs Zuko (fire)
                 if (computerFighter == fighter.firebender) {
-                    computerFighter.hp = (computerFighter.hp - (yourFighter.ap + 5));
+                    var j = Math.floor(Math.random() * 6);
+                    var i = Math.floor(Math.random() * 5);
+                    if (j < 5) {
+                        computerFighter.hp = (computerFighter.hp - (yourFighter.ap + 5));
+                        yourFighter.ap = yourFighter.ap + 5;
+                        $("#stats-you").text("Aang attacked Zuko with " + yourFighter.ap + " ap!")
+                    } else {
+                        $("#stats-you").text("Aang's attack missed!");
+                    }
+                    if (i<4){
                     yourFighter.hp = (yourFighter.hp - (computerFighter.cap+3));
-                    yourFighter.ap = yourFighter.ap + 6;
+                    $("#stats-computer").text("Zuko counter-attacked with " + (computerFighter.cap+3) + " ap!")
+                    }else {
+                        $("#stats-computer").text("Zuko's counter-attack missed!")
+                    }
                 }
-                // vs earth
+                // vs Toph (earth)
                 if (computerFighter == fighter.earthbender) {
-                    computerFighter.hp = (computerFighter.hp - (yourFighter.ap - 5));
+                    var j = Math.floor(Math.random() * 6);
+                    var i = Math.floor(Math.random() * 5);
+                    if (j < 5) {
+                        computerFighter.hp = (computerFighter.hp - (yourFighter.ap - 5));
+                        yourFighter.ap = yourFighter.ap + 5;
+                        $("#stats-you").text("Aang attacked Toph with " + yourFighter.ap + " ap!")
+                    } else {
+                        $("#stats-you").text("Aang's attack missed!");
+                    }
+                    if (i<4){
                     yourFighter.hp = (yourFighter.hp - computerFighter.cap);
-                    yourFighter.ap = yourFighter.ap + 6;
+                    $("#stats-computer").text("Toph counter-attacked with " + computerFighter.cap + " ap!")
+                    }else {
+                        $("#stats-computer").text("Toph's counter-attack missed!")
+                    }
                 }
                 
             }
-            // air vs fire
-            // air vs earth
-            // water vs fire
-            // water vs earth
-            // water vs air
-            // fire vs earth
-            // fire vs air
-            // fire vs water
-            // earth vs air
-            // earth vs water
-            // earth vs fire
+            // if you're Katara (water)=============================================================================
+            if (yourFighter == fighter.waterbender) {
+                // vs Zuko (fire)+++++++++++++++++++++++++++++++++++++++++++
+                if (computerFighter == fighter.firebender) {
+                    var j = Math.floor(Math.random() * 6);
+                    var i = Math.floor(Math.random() * 5);
+                    if (j < 5) {
+                        computerFighter.hp = (computerFighter.hp - (yourFighter.ap + 5));
+                        yourFighter.ap = yourFighter.ap + 5;
+                        $("#stats-you").text("Katara attacked Zuko with " + yourFighter.ap + " ap!")
+                    } else {
+                        $("#stats-you").text("Katara's attack missed!");
+                    }
+                    if (i < 4) {
+                    yourFighter.hp = (yourFighter.hp - (computerFighter.cap-5));
+                    $("#stats-computer").text("Zuko counter-attacked with " + (computerFighter.cap-5) + " ap!")
+                    } else {
+                        $("#stats-computer").text("Zuko's counter-attack missed!")
+                    }
+                }
+                // vs Toph (earth)+++++++++++++++++++++++++++++++++++++++++++
+                if (computerFighter == fighter.earthbender) {
+                    var j = Math.floor(Math.random() * 6);
+                    var i = Math.floor(Math.random() * 5);
+                    if (j < 5) {
+                        computerFighter.hp = (computerFighter.hp - (yourFighter.ap + 5));
+                        yourFighter.ap = yourFighter.ap + 5;
+                        $("#stats-you").text("Katara attacked Toph with " + yourFighter.ap + " ap!")
+                    } else {
+                        $("#stats-you").text("Katara's attack missed!");
+                    }
+                    if (i<4){
+                    yourFighter.hp = (yourFighter.hp - (computerFighter.cap+3));
+                    $("#stats-computer").text("Toph counter-attacked with " + (computerFighter.cap+3) + " ap!")
+                    }else {
+                        $("#stats-computer").text("Toph's counter-attack missed!")
+                    }
+                }
+                // vs Aang (air)+++++++++++++++++++++++++++++++++++++++++++
+                if (computerFighter == fighter.airbender) {
+                    var j = Math.floor(Math.random() * 6);
+                    var i = Math.floor(Math.random() * 5);
+                    if (j < 5) {
+                        computerFighter.hp = (computerFighter.hp - (yourFighter.ap - 5));
+                        yourFighter.ap = yourFighter.ap + 5;
+                        $("#stats-you").text("Katara attacked Aang with " + yourFighter.ap + " ap!")
+                    } else {
+                        $("#stats-you").text("Katara's attack missed!");
+                    }
+                    if (i<4){
+                    yourFighter.hp = (yourFighter.hp - computerFighter.cap);
+                    $("#stats-computer").text("Aang counter-attacked with " + computerFighter.cap + " ap!")
+                    }else {
+                        $("#stats-computer").text("Aang's counter-attack missed!")
+                    }
+                }
+                
+            }
+            // if you're Zuko (fire)=================================================================================
+            if (yourFighter == fighter.firebender) {
+                // vs Toph (earth)+++++++++++++++++++++++++++++++++++++++++++
+                if (computerFighter == fighter.earthbender) {
+                    var j = Math.floor(Math.random() * 6);
+                    var i = Math.floor(Math.random() * 5);
+                    if (j < 5) {
+                        computerFighter.hp = (computerFighter.hp - (yourFighter.ap + 5));
+                        yourFighter.ap = yourFighter.ap + 5;
+                        $("#stats-you").text("Zuko attacked Toph with " + yourFighter.ap + " ap!")
+                    } else {
+                        $("#stats-you").text("Zuko's attack missed!");
+                    }
+                    if (i < 4) {
+                    yourFighter.hp = (yourFighter.hp - (computerFighter.cap-5));
+                    $("#stats-computer").text("Toph counter-attacked with " + (computerFighter.cap-5) + " ap!")
+                    } else {
+                        $("#stats-computer").text("Toph's counter-attack missed!")
+                    }
+                }
+                // vs Aang (air)+++++++++++++++++++++++++++++++++++++++++++
+                if (computerFighter == fighter.airbender) {
+                    var j = Math.floor(Math.random() * 6);
+                    var i = Math.floor(Math.random() * 5);
+                    if (j < 5) {
+                        computerFighter.hp = (computerFighter.hp - (yourFighter.ap + 5));
+                        yourFighter.ap = yourFighter.ap + 5;
+                        $("#stats-you").text("Zuko attacked Aang with " + yourFighter.ap + " ap!")
+                    } else {
+                        $("#stats-you").text("Zuko's attack missed!");
+                    }
+                    if (i<4){
+                    yourFighter.hp = (yourFighter.hp - (computerFighter.cap+3));
+                    $("#stats-computer").text("Aang counter-attacked with " + (computerFighter.cap+3) + " ap!")
+                    }else {
+                        $("#stats-computer").text("Aang's counter-attack missed!")
+                    }
+                }
+                // vs Katara (water)+++++++++++++++++++++++++++++++++++++++++++
+                if (computerFighter == fighter.waterbender) {
+                    var j = Math.floor(Math.random() * 6);
+                    var i = Math.floor(Math.random() * 5);
+                    if (j < 5) {
+                        computerFighter.hp = (computerFighter.hp - (yourFighter.ap - 5));
+                        yourFighter.ap = yourFighter.ap + 5;
+                        $("#stats-you").text("Zuko attacked Katara with " + yourFighter.ap + " ap!")
+                    } else {
+                        $("#stats-you").text("Zuko's attack missed!");
+                    }
+                    if (i<4){
+                    yourFighter.hp = (yourFighter.hp - computerFighter.cap);
+                    $("#stats-computer").text("Katara counter-attacked with " + computerFighter.cap + " ap!")
+                    }else {
+                        $("#stats-computer").text("Katara's counter-attack missed!")
+                    }
+                }
+                
+            }
+            // if you're Toph (earth)=================================================================================
+            if (yourFighter == fighter.earthbender) {
+                // vs Aang (air)+++++++++++++++++++++++++++++++++++++++++++
+                if (computerFighter == fighter.airbender) {
+                    var j = Math.floor(Math.random() * 6);
+                    var i = Math.floor(Math.random() * 5);
+                    if (j < 5) {
+                        computerFighter.hp = (computerFighter.hp - (yourFighter.ap + 5));
+                        yourFighter.ap = yourFighter.ap + 5;
+                        $("#stats-you").text("Toph attacked Aang with " + yourFighter.ap + " ap!")
+                    } else {
+                        $("#stats-you").text("Toph's attack missed!");
+                    }
+                    if (i < 4) {
+                    yourFighter.hp = (yourFighter.hp - (computerFighter.cap-5));
+                    $("#stats-computer").text("Aang counter-attacked with " + (computerFighter.cap-5) + " ap!")
+                    } else {
+                        $("#stats-computer").text("Aang's counter-attack missed!")
+                    }
+                }
+                // vs Katara (water)+++++++++++++++++++++++++++++++++++++++++++
+                if (computerFighter == fighter.waterbender) {
+                    var j = Math.floor(Math.random() * 6);
+                    var i = Math.floor(Math.random() * 5);
+                    if (j < 5) {
+                        computerFighter.hp = (computerFighter.hp - (yourFighter.ap + 5));
+                        yourFighter.ap = yourFighter.ap + 5;
+                        $("#stats-you").text("Toph attacked Katara with " + yourFighter.ap + " ap!")
+                    } else {
+                        $("#stats-you").text("Toph's attack missed!");
+                    }
+                    if (i<4){
+                    yourFighter.hp = (yourFighter.hp - (computerFighter.cap+3));
+                    $("#stats-computer").text("Katara counter-attacked with " + (computerFighter.cap+3) + " ap!")
+                    }else {
+                        $("#stats-computer").text("Katara's counter-attack missed!")
+                    }
+                }
+                // vs Zuko (fire)+++++++++++++++++++++++++++++++++++++++++++
+                if (computerFighter == fighter.firebender) {
+                    var j = Math.floor(Math.random() * 6);
+                    var i = Math.floor(Math.random() * 5);
+                    if (j < 5) {
+                        computerFighter.hp = (computerFighter.hp - (yourFighter.ap - 5));
+                        yourFighter.ap = yourFighter.ap + 5;
+                        $("#stats-you").text("Toph attacked Zuko with " + yourFighter.ap + " ap!")
+                    } else {
+                        $("#stats-you").text("Toph's attack missed!");
+                    }
+                    if (i<4){
+                    yourFighter.hp = (yourFighter.hp - computerFighter.cap);
+                    $("#stats-computer").text("Zuko counter-attacked with " + computerFighter.cap + " ap!")
+                    }else {
+                        $("#stats-computer").text("Zuko's counter-attack missed!")
+                    }
+                }
+                
+            }
+            
+     
+            // win/lose conditions
            
             $(".yourHP").text("hp: " + yourFighter.hp);
             $(".computerHP").text("chp: " + computerFighter.hp);
@@ -172,6 +366,7 @@ $("#start-text").text("Select your fighter!");
           
         }
     })
+    // reset game button
     $('#restart').click(function() {
         location.reload();
     });
